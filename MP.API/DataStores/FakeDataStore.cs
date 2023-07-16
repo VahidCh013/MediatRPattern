2c1578668f6411e2774145982fbae54b511312fb
Vahid Cheshmy <vahid.cheshmy@syzygy.de>
2023-07-16 15:19:34 +0200

    Fix: minor issue.

--- MP.API/DataStores/FakeDataStore.cs
+++ MP.API/DataStores/FakeDataStore.cs
@@ -7,8 +7,8 @@ public class FakeDataStore
     private static readonly List<Book> Books = new()
     {
         new Book {Id = 1,Name="C#"},
-        new Book {Id = 1,Name="ASP.NET core"},
-        new Book {Id = 1,Name="SQL Server "},
+        new Book {Id = 2,Name="ASP.NET core"},
+        new Book {Id = 3,Name="SQL Server"}
     };
 
     public async Task<Book> AddBook(Book book)
